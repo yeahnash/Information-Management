cb427e4fff8b761b00af296a70a906825dd04d44
ychiong <ychiong@JF607B-007.addunet.ph>
2026-08-18 09:32:57 +0800

    Draft

---
+++ src/main/java/com/mycompany/enrollmentsystem/EnrollmentSystem.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.enrollmentsystem;
+
+/**
+ *
+ * @author ychiong
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class EnrollmentSystem {
+    
+    Connection con;
+    Statement st;
+    static ResultSet rs;
+
+    public static void main(String[] args) {
+        StudentsForm a = new StudentsForm();
+        a.showrecords();
+        a.setVisible(true);
+    }
+    
+    public boolean DBConnect(){
+
+        try{
+
+         Class.forName("com.mysql.jdbc.Driver");
+
+          con = DriverManager.getConnection("jdbc:mysql://localhost:3306/enrollmentsystem?zeroDateTimeBehavior=CONVERT_TO_NULL","root","root");
+
+          st = con.createStatement();  
+
+          System.out.println("Connected to database:");
+
+          }catch (Exception ex) {
+
+          System.out.print(ex);    
+
+     }
+    return true;
+    }
+}
---
+++ src/main/java/com/mycompany/enrollmentsystem/Students.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.enrollmentsystem;
+
+/**
+ *
+ * @author ychiong
+ */
+public class Students {
+    public void newstudent(int studID, String studName, String studAdd, String studCrs, String studGender, String yrLvl){
+        String query = "insert into Students values ("+ studID + ",'"+ studName +"','"+ studAdd );
+        b.st.executeUpdate(query);
+    }
+    public void delete_student(){
+        
+    }
+    public void update_student(){
+        
+    }
+}
